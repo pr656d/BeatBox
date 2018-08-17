9dc1698c3edeba072c23803eeab7d71f8e305acd
prm14 <prempatel40p@gmail.com>
2018-08-17 23:23:12 +0530

    initial commit

---
+++ app/src/main/java/com/premp/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package com.premp.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    // abstract declarations.
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();    // it's equal to -> new Fragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+    } // onCreate end.
+}
---
+++ app/src/main/java/com/premp/beatbox/Sound.java
@@ -0,0 +1,23 @@
+package com.premp.beatbox;
+
+public class Sound {
+    public static final String TAG = "sound";
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String fileName = components[components.length - 1];
+        mName = fileName.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
