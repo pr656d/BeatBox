1fa9d1ddfdbfd691eac080d418cd685fa35d795c
prm14 <prempatel40p@gmail.com>
2018-08-24 09:40:38 +0530

    test added

--- app/src/main/java/com/premp/beatbox/SoundViewModel.java
+++ app/src/main/java/com/premp/beatbox/SoundViewModel.java
@@ -1,6 +1,9 @@
 package com.premp.beatbox;
 
-public class SoundViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
     private Sound mSound;
     private BeatBox mBeatBox;
 
@@ -8,6 +11,7 @@ public class SoundViewModel {
         mBeatBox = beatBox;
     }
 
+    @Bindable
     public String getTitle() {
         return mSound.getName();
     }
@@ -18,5 +22,10 @@ public class SoundViewModel {
 
     public void setSound(Sound sound) {
         mSound = sound;
+        notifyChange();
+    }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
     }
 }
