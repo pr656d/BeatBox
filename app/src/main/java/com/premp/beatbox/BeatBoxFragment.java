56a47aceb20f9207403a29d050bdd8e0ef8fe1f2
prm14 <prempatel40p@gmail.com>
2018-09-01 02:25:49 +0530

    Playing sound

--- app/src/main/java/com/premp/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/premp/beatbox/BeatBoxFragment.java
@@ -45,6 +45,12 @@ public class BeatBoxFragment extends Fragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
     private class SoundHolder extends RecyclerView.ViewHolder {
         private ListItemSoundBinding mBinding;
 
